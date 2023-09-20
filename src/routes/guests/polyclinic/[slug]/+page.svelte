6198f4873817ee2dd23a92db504aebad508c5d0e
“gevera” <skryonline@gmail.com>
2023-09-20 21:06:25 +0500

    Fix cache issues. Make sure not to refresh [age from local cache

--- src/routes/guests/polyclinic/[slug]/+page.svelte
+++ src/routes/guests/polyclinic/[slug]/+page.svelte
@@ -3,38 +3,45 @@
 
 	export let data: PageData;
 	$: ({ main_url, page } = data);
+
 </script>
 
 <svelte:head>
-	{@html page?.head}
+	{@html page && page?.head ? page?.head : 'Зеленая Роща'}
 </svelte:head>
 
-<div
-	class="hero min-h-[50vh] mt-20"
-	style={`background-image: url(${
-		main_url + (page?.hero_image?.formats?.large?.url || page?.hero_image?.formats?.medium?.url)
-	});`}
->
-	<div class="hero-overlay bg-opacity-60" />
-	<div class="hero-content text-center text-neutral-content">
-		<div class="max-w-md">
-			<h1 class="mb-5 text-3xl md:text-5xl font-bold font-condensed text-white max-w-lg break-words">
-				{page?.title}
-			</h1>
+{#if page}
+	<div
+		class="hero min-h-[50vh] mt-20"
+		style={`background-image: url(${
+			main_url + (page?.hero_image?.formats?.large?.url || page?.hero_image?.formats?.medium?.url)
+		});`}
+	>
+		<div class="hero-overlay bg-opacity-60" />
+		<div class="hero-content text-center text-neutral-content">
+			<div class="max-w-md">
+				<h1
+					class="mb-5 text-3xl md:text-5xl font-bold font-condensed text-white max-w-lg break-words"
+				>
+					{page?.title}
+				</h1>
+			</div>
 		</div>
 	</div>
-</div>
-<section class="container p-4 mx-auto max-w-screen-xl my-12 overflow-hidden">
-	<div class="gap-8 flex flex-col lg:flex-row items-start">
-		<div class="flex-grow">
-			{@html page.body}
+	<section class="container p-4 mx-auto max-w-screen-xl my-12 overflow-hidden">
+		<div class="gap-8 flex flex-col lg:flex-row items-start">
+			<div class="flex-grow">
+				{@html page?.body}
+			</div>
+			{#if page?.secondary_image}
+				<div class="lg:max-w-md w-full place-self-center lg:place-self-start">
+					<img
+						src={main_url + page?.secondary_image.formats?.medium?.url}
+						alt={page?.title}
+						class="w-full rounded-lg object-cover object-center"
+					/>
+				</div>
+			{/if}
 		</div>
-		<div class="lg:max-w-md w-full place-self-center lg:place-self-start">
-			<img
-				src={main_url + page.secondary_image.formats?.medium?.url}
-				alt={page?.title}
-				class="w-full rounded-lg object-cover object-center"
-			/>
-		</div>
-	</div>
-</section>
+	</section>
+{/if}
--- src/routes/guests/polyclinic/[slug]/+page.ts
+++ src/routes/guests/polyclinic/[slug]/+page.ts
@@ -4,28 +4,21 @@ import type { PageLoad } from './$types';
 
 export const load = (async ({ params: { slug } }) => {
     const main_url = get(siteData).api_url;
-    const page = get(pages).find(p => p.slug == slug);
-    if (page == -1) {
-        const url = new URL('dynamic-pages', main_url)
-        url.searchParams.append('slug', slug)
-        try {
-            const res = await fetch(url);
-            if (res.ok) {
-                const [page, _] = await res.json();
-                console.log(page);
+    const url = new URL('dynamic-pages', main_url)
+    url.searchParams.append('slug', slug)
+    try {
+        const res = await fetch(url);
+        if (res.ok) {
+            const [page, data] = await res.json();
+            console.log(page);
 
-                return { page, main_url };
-            }
-
-        } catch (error) {
-            console.log(error);
-            return {
-                page: {}, main_url
-            }
+            return { page, main_url };
         }
-    } else {
+
+    } catch (error) {
+        console.log(error);
         return {
-            page, main_url
+            page: {}, main_url
         }
     }
 }) satisfies PageLoad;
